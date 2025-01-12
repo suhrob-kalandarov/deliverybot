dde488bb4e23caae44c459942f88f5e5015f300e
Suhrob Qalandarov <suhrob_kalandarov@outlook.com>
2025-01-12 12:25:17 +0500

    Update DB.java

--- src/main/java/org/example/database/DB.java
+++ src/main/java/org/example/database/DB.java
@@ -11,19 +11,15 @@ import java.util.*;
 
 public interface DB {
     List<User> USERS = new ArrayList<>();
-    Map<Long, List<OrderedItem>> LONG_LIST_MAP = new HashMap<>();
-
     Map<Product, Integer> CART = new HashMap<>();
-
     List<Location> LOCATIONS = new ArrayList<>();
-
+    List<OrderedItem> ORDERED_ITEMS = new ArrayList<>();
     List<Order> ORDERS = new ArrayList<>();
-
+    
     List<Category> CATEGORIES = new ArrayList<>(List.of(
             new Category("Pizzas🍕"),
             new Category("Burgers🍔"),
             new Category("Lavashes🌯"),
-            //new Category("Sandwich🥪"),
             new Category("Ichimliklar🥤")
     ));
 
@@ -43,9 +39,4 @@ public interface DB {
             new Product("Lavash", 3, 32_000),
             new Product("", 3, 32_000)
     ));
-    List<OrderedItem> ORDERED_ITEMS = new ArrayList<>();
-
-    /*static List<Product> getProductsByCategory(Category category) {
-        return PRODUCTS.stream().filter(product -> Objects.equals(product.getCategoryId(), category.getId())).collect(Collectors.toList());
-    }*/
 }
